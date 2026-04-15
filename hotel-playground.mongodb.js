d61b587c95a4105319014aeb2e73504d01505f02
pedrocoelho04 <bunnybrazilian@gmail.com>
2026-04-15 20:33:25 -0300

    Update: Incrementado ultimas consultas utilizando operadores logicos ($and, $or, $not, $nand, $nor, etc)

--- hotel-playground.mongodb.js
+++ hotel-playground.mongodb.js
@@ -230,8 +230,8 @@ db.reserva.insertMany([
   {
     quarto: 109,
     hospede_cpf: "67584939987",
-    data_inicio: new Date("2026-12-28"),
-    data_fim: new Date("2025-01-02"),
+    data_inicio: new Date("2026-10-14"),
+    data_fim: new Date("2026-11-02"),
     valor_total: 6500,
     servicos_adicionais: ["festa de reveillon", "open bar"]
   },
@@ -249,71 +249,78 @@ db.reserva.insertMany([
 // 3. CONSULTAS COM OPERADORES DE COMPARAÇÃO
 // --------------------------------------------------
 
-// 3.1 $gt - (GET) Serve para procurar por um valor exatamente igual ao solicitado 
-db.hospede.find({ nome: { $gt: "Fernanda Alves" } }).pretty()
+// 3.1 $gt - (Greater Than) Serve para procurar por um valor maior que ao solicitado 
+db.hospede.find({ idade: { $gt: 18 } }).pretty()
 
-// 3.2 $lt - (Less Then) Serve para procurar por um valor menor que o valor solicitado
+// 3.2 $lt - (Less Than) Serve para procurar por um valor menor que o valor solicitado
 db.hospede.find({ idade: { $lt: 18 } }).pretty()
 
-// 3.3 $gte - (Greater Then) Serve para procurar por um valor maior ou igual que o valor solicitado
+// 3.3 $gte - (Greater Than or Equal) Serve para procurar por um valor maior ou igual que o valor solicitado
 db.reserva.find({ valor_total: { $gte: 3000 } }).pretty()
 
-// 3.4 $lte - (Less or Equal) Serve para procurar por um valor menor ou igual ao valor solicitado
+// 3.4 $lte - (Less Than or Equal) Serve para procurar por um valor menor ou igual ao valor solicitado
 db.reserva.find({ valor_total: { $lte: 6000 } }).pretty()
 
 // 3.5 $in - (In array) Serve para procurar por um valor dentro de uma Array
-db.hospede.find({ servicos_adicionais: { $in: ["wi-fi premium"] } }).pretty()
+db.reserva.find({ servicos_adicionais: { $in: ["wi-fi premium", "frigobar"] } } ).pretty()
 
 // 3.6 $ne - (Not Equal) Serve para procurar valores diferentes do especificado
 db.hospede.find({ genero: { $ne: false } }).pretty()
+
+// 3.7 $eq - (Equal) Serve para procurar valores iguais ao especificado
+db.hospede.find({ genero: { $eq: true } }).pretty()
 
 // --------------------------------------------------
 // 4. CONSULTAS COM OPERADORES LÓGICOS
 // --------------------------------------------------
 
-// 4.1 $and (implícito) - [explicação]
+// 4.1 $and (implícito) - Serve para combinar multiplas condições que devem estar verdadeiras de forma simples
 db.hospede.find({ 
-    campo1: valor1, 
-    campo2: valor2 
+    genero: false, 
+    idade: { $gte: 18 } 
 }).pretty()
 
-// 4.2 $and (explícito) - [explicação]
+// 4.2 $and (explícito) - Serve para combinar multiplas condições que devem estar verdadeiras de forma mais complexa, podendo ser mais exata em sua posição de objeto
 db.hospede.find({ 
     $and: [
-        { campo1: valor1 },
-        { campo2: valor2 }
+        { genero: true },
+        { idade: { $gte: 40 } }
     ] 
 }).pretty()
 
-// 4.3 $or - [explicação]
-db.hospede.find({ 
+// 4.3 $or - Serve para combinar multiplas condições onde pelo menos uma delas devem ser verdadeira.
+db.reserva.find({ 
     $or: [
-        { campo1: valor1 },
-        { campo2: valor2 }
+        { servicos_adicionais: "estacionamento" },
+        { servicos_adicionais: "aluguel de carro" }
     ] 
 }).pretty()
 
-// 4.4 $nor - [explicação]
-db.hospede.find({ 
+// 4.4 $nor - Serve para combinar multiplas condições onde nenhuma delas deve ser verdadeira.
+db.reserva.find({ 
     $nor: [
-        { campo1: valor1 },
-        { campo2: valor2 }
+        { servicos_adicionais: "frigobar" },
+        { servicos_adicionais: "open bar" }
     ] 
 }).pretty()
 
-// 4.5 $not - [explicação]
-db.hospede.find({ 
-    campo: { $not: { $gt: valor } }
+// 4.5 $not - Serve para negar uma unica condição para que ela não seja verdadeira.
+db.reserva.find({ 
+    valor_total: { $not: { $lte: 1000 } }
 }).pretty()
 
-// 4.6 Combinação ($and + $or) - [explicação]
-db.hospede.find({
+// 4.6 Combinação ($and + $or) - Combinando o and e o or para criar uma consulta mais complexa, onde verifica uma reserva com algum serviço alimenticio dentro da data de outubro ou novembro.
+db.reserva.find({
     $and: [
-        { campo1: valor1 },
-        { $or: [
-            { campo2: valor2 },
-            { campo3: valor3 }
-        ]}
+      { data_inicio: { $gt: new Date("2026-10-01") } },
+      { data_fim: { $lt: new Date("2026-11-31") } },
+      { $or : [
+          { servicos_adicionais: "café da manhã" },
+          { servicos_adicionais: "ceia de natal" },
+          { servicos_adicionais: "festa de reveillon" },
+          { servicos_adicionais: "open bar" },
+        ] 
+      }
     ]
 }).pretty()
 
